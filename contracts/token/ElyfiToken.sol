4df9527c1fc970544265f548e1d4a2920ecc3ca3
donguk.seo <donguk.seo@modori.io>
2021-06-03 16:34:16 +0900

    Impl snapshot & snapshot maker role

--- contracts/token/ElyfiToken.sol
+++ contracts/token/ElyfiToken.sol
@@ -1,12 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
+import '../access/ElyfiAccessControl.sol';
 import '@openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol';
+import '@openzeppelin/contracts/access/AccessControl.sol';
 
-contract ElyfiToken is ERC20Snapshot {
+contract ElyfiToken is ERC20Snapshot, ElyfiAccessControl {
   uint256 internal constant TOTAL_SUPPLY = 100000000 ether;
 
   constructor() ERC20('ELFI Token', 'ELFI') {
     _mint(msg.sender, TOTAL_SUPPLY);
+    _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+  }
+
+  function snapshot() external onlySnapshotMaker returns (uint256) {
+    return _snapshot();
   }
 }
